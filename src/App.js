ea5cb6f808ac058d835cb4b4613a76d55a0a27a5
Publikwerker <jason.michael.hoffman@gmail.com>
2019-03-06 08:59:13 -0800

    added basic DOM, button events are captured

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      input: [],
+      results: '',
+      searchType: '',
+    }
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    console.log(e);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Searching Tree Traversal Drills</h1>
+          <div className="results">
+          </div>
         </header>
+        <main role='main'>
+          <div className="input-field">
+          <form className="input-form">
+            <textarea 
+              className="array-input" 
+              rows='10'
+              cols='50'
+              ></textarea>
+            <button type="submit"
+              value="linear-search" 
+              className="linear-search"
+              onClick={e => this.handleSubmit(e)}>Linear Search</button>
+            <button type="submit" 
+              value="binary-search" 
+              className="binary-search"
+              onClick={e => this.handleSubmit(e)}>Binary Search</button>
+          </form>
+          </div>
+        
+        </main>
       </div>
     );
   }
