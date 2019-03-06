1f7b782fe1bdc87f7a9806e3360a206e7ac9afd2
nicknotj <nicknotjustin@gmail.com>
2019-03-06 11:43:27 -0600

    We spent the entire morning getting react to work.

--- src/App.js
+++ src/App.js
@@ -11,9 +11,35 @@ class App extends Component {
     }
   }
 
-  handleSubmit(e){
+  handleSubmit = (e) => {
+    //Is to grab the textarea value AND the search type
+    //Thought: Put handleSubmit on form
     e.preventDefault();
-    console.log(e);
+    //Below is the value of the textArea
+    let value = e.currentTarget[0].value;
+    this.setState({
+      input: value
+    })
+    
+  }
+  handleClick = (e) => {
+    console.log(this.props);
+    console.log(e.target.value);
+    let value = e.target.value;
+    this.setState({
+      searchType: value
+    })
+  }
+
+  returnState = () => {
+    let inputReturn = this.state.input;
+    let searchReturn = this.state.searchType;
+    return(
+      <div>
+        <p>{inputReturn}</p>
+        <p>{searchReturn}</p>
+      </div>
+    )
   }
 
   render() {
@@ -22,24 +48,26 @@ class App extends Component {
         <header className="App-header">
           <h1>Searching Tree Traversal Drills</h1>
           <div className="results">
+          {this.returnState()}
           </div>
         </header>
         <main role='main'>
           <div className="input-field">
-          <form className="input-form">
+          <form onSubmit={this.handleSubmit} className="input-form">
             <textarea 
               className="array-input" 
               rows='10'
               cols='50'
               ></textarea>
             <button type="submit"
+              onClick={this.handleClick}
               value="linear-search" 
               className="linear-search"
-              onClick={e => this.handleSubmit(e)}>Linear Search</button>
-            <button type="submit" 
+              >Linear Search</button>
+            <button onClick={this.handleClick} type="submit" 
               value="binary-search" 
               className="binary-search"
-              onClick={e => this.handleSubmit(e)}>Binary Search</button>
+              >Binary Search</button>
           </form>
           </div>
         
